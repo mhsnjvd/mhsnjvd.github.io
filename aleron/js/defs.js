674ae4f04d52099e1bfc56cc3441d9e56c1ff4b1
Mohsin Javed <mhsnjvd@gmail.com>
2015-06-19 16:12:35 +0100

    getting to grips with java script.

---
+++ aleron/js/defs.js
@@ -0,0 +1,9 @@
+//This is a file of definitions and various variables to be
+//used by the web pages
+// Mohsin Javed, June 19, 2015
+//
+// The global variables will be named with underscores.
+
+var companyName = "Aleron Group";
+var clientName = "Bernardos";
+var companyIconURL = "./favicon.jpg";
