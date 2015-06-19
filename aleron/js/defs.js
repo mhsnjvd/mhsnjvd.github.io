c6dff36d5ae03d41cabd9868803813cdaf99d969
Mohsin Javed <mhsnjvd@gmail.com>
2015-06-19 19:08:48 +0100

    adding carousel page for aleron.

--- aleron/js/defs.js
+++ aleron/js/defs.js
@@ -5,5 +5,24 @@
 // The global variables will be named with underscores.
 
 var companyName = "Aleron Group";
+var companyURL = "http://www.alerongroup.com";
 var clientName = "Bernardos";
 var companyIconURL = "./favicon.jpg";
+
+//******************************************
+// Carousel Page definitions
+// ****************************************//
+
+// How many pictures do we want to show in the background
+var nBckGrounds = 4;
+var bckGroundURL = [];
+
+var i = 0;
+ 
+for ( i = 0;i < nBckGrounds; i++ )
+{
+    bckGroundURL.push(i);
+}
+console.write(bckGroundURL);
+
+
